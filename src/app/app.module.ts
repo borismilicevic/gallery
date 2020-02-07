ee2db80009e6778fb9e73c8137c354d5ac69e055
Boris Milicevic <bmilicevic@domain.local>
2020-02-07 12:36:15 +0100

    Login component
    - Created LoginCompoent with desired template and style.
    - Created User model.
    - Created empty authentication service and injected it as
    dependency into LoginCompoent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatInputModule} from '@angular/material/input'; 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -11,8 +16,13 @@ import { AppRoutingModule } from './app-routing.module';
     LoginComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/User';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.user = new User();
+  }
+
+  loginUser()
+  {
+    this.authenticationService.login();
+    console.log(this.user);
   }
 
 }
